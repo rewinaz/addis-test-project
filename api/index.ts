daeed7f6abad20b0d9c34ae5fb2f6e5d578da9cf
Rewina <zerourewina9@gmail.com>
2023-04-29 03:07:32 +0300

    cors issue fix

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 
-const PORT = 3000 | Number(process.env.PORT);
+const PORT = Number(process.env.PORT) || 3000;
 
 const app = express();
 
