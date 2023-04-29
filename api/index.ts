320fec205a62d8fb2dd76587c0f8d1810295e227
Rewina <zerourewina9@gmail.com>
2023-04-29 10:09:31 +0300

    cors allowed origin set to *

--- api/index.ts
+++ api/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Content-Type",
